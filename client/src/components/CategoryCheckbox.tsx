ebcf8797207180dd224f742d04bb6a6624afd766
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-05 20:47:43 +0100

    create category checkbox

---
+++ client/src/components/CategoryCheckbox.tsx
@@ -0,0 +1,26 @@
+type CategoryCheckboxProps = {
+	idFor: string
+	labelContent: string
+	checked: boolean
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function CategoryCheckbox({ idFor, labelContent, checked, onChange }: CategoryCheckboxProps) {
+	return (
+		<div className="flex items-center gap-3">
+			<input
+				type="checkbox"
+				id={idFor}
+				checked={checked}
+				onChange={onChange}
+				className="w-5 h-5 accent-taupe-200 cursor-pointer focus:ring-0"
+			/>
+
+			<label htmlFor={idFor} className="text-xl text-taupe-200 select-none cursor-pointer">
+				{labelContent}
+			</label>
+		</div>
+	)
+}
+
+export default CategoryCheckbox
