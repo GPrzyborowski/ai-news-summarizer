d8ddc8ec76b68cc677dcef4cb60cb2feaa1beb4d
Krzysztof Stasiak <krzsta@proton.me>
2026-03-09 19:59:09 +0100

    Add existsByEmail method to UserRepository

--- server/src/main/java/com/github/krzsta/server/repository/UserRepository.java
+++ server/src/main/java/com/github/krzsta/server/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 
     Optional<AppUser> findByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
