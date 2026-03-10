4f6ef0b8429237b70a904d9004496f59096d785e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-10 03:58:53 +0100

    add user registration function

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,13 +1,37 @@
 import InputBox from './InputBox'
 import { useState } from 'react'
+import { API_URL } from '../config/env'
 
 function RegisterForm() {
 	const [login, setLogin] = useState('')
 	const [password, setPassword] = useState('')
 	const [email, setEmail] = useState('')
 
+	const REGISTER_ENDPOINT = `${API_URL}/auth/register`
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const res = await fetch(REGISTER_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: login,
+				email: email,
+				password: password,
+			}),
+		})
+		if (!res.ok) {
+			console.error(res)
+			return
+		}
+		const data = await res.json()
+		console.log(data);
+	}
+
 	return (
-		<form action="">
+		<form onSubmit={handleSubmit}>
 			<InputBox
 				htmlFor="email"
 				labelText="Email"
@@ -32,8 +56,10 @@ function RegisterForm() {
 				value={password}
 				onChange={e => setPassword(e.target.value)}
 			/>
-			<div className='flex justify-center'>
-				<button type="submit" className="border border-taupe-200 mt-8 px-8 py-4 cursor-pointer text-2xl hover:text-[#242424] hover:bg-taupe-200 duration-100 ease-in">
+			<div className="flex justify-center">
+				<button
+					type="submit"
+					className="border border-taupe-200 mt-8 px-8 py-4 cursor-pointer text-2xl hover:text-[#242424] hover:bg-taupe-200 duration-100 ease-in">
 					Register
 				</button>
 			</div>
