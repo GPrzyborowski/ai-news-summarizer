c24350df8282d9c487f97f3134c1815bc3eee895
Krzysztof Stasiak <krzsta@proton.me>
2026-03-14 00:36:12 +0100

    add: appuser class and dependencies

---
+++ server/src/main/java/com/github/krzsta/server/user/AppUser.java
@@ -0,0 +1,68 @@
+package com.github.krzsta.server.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "createad_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    public AppUser(String username, String email, String passwordHash) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPasswordHash() {
+        return this.passwordHash;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
